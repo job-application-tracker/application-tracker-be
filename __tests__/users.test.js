821ffc063d90dc4146921aba4c9e95a8cbbf065a
Denver McCarthy <dmichaelspdx@gmail.com>
2022-07-13 11:15:47 -0700

    added signInAndUp function for testing, /me test

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -3,6 +3,21 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const signInAndUp = async () => {
+  try {
+    const mockUser = {
+      email: 'testing@email.com',
+      password: 'password',
+    };
+    const agent = await request.agent(app);
+    const user = agent.post('/api/v1/users').send(mockUser);
+    await agent.post('/api/v1/users/sessions').send(mockUser);
+    return [agent, user];
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -17,6 +32,21 @@ describe('backend-express-template routes', () => {
     expect(res.body).toEqual({
       id: expect.any(String),
       email: mockUser.email,
+      appGoal: expect.any(Number),
+      networkGoal: expect.any(Number),
+      meetupGoal: expect.any(Number),
+      linkedinGoal: expect.any(Number),
+      codeGoal: expect.any(Number),
+    });
+  });
+
+  test('GET to /api/v1/users/me should return a signed in user`s information ', async () => {
+    const [agent] = await signInAndUp();
+    const me = await agent.get('/api/v1/users/me');
+    expect(me.status).toBe(200);
+    expect(me.body).toEqual({
+      id: expect.any(String),
+      email: 'testing@email.com',
       appGoal: expect.any(Number),
       networkGoal: expect.any(Number),
       meetupGoal: expect.any(Number),
