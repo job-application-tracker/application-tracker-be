26c4401eb67cdd5f9d746677a9d46d81ad6c316c
Mary Martinez <mary.diana.martinez@gmail.com>
2022-07-13 16:59:25 -0700

    authorize middleware added

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const signInAndUp = async () => {
   try {
@@ -62,6 +63,31 @@ describe('backend-express-template routes', () => {
 
     expect(out.status).toBe(200);
     expect(out.body).toEqual({ message: 'Successfully signed out.' });
+  });
+  test('PUT to /api/v1/users/:id should update user profile', async () => {
+    const [agent, user] = await signInAndUp();
+    const res = await agent.put(`/api/v1/users/${user.body.id}`).send({
+      appGoal: 2,
+      networkGoal: 5
+    });
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'testing@email.com',
+      appGoal: 2,
+      networkGoal: 5,
+      meetupGoal: expect.any(Number),
+      linkedinGoal: expect.any(Number),
+      codeGoal: expect.any(Number),
+    });
+  });
+  test('PUT /api/v1/users/:id should return a 403 if the user is not authorized', async () => {
+    const [agent] = await signInAndUp();
+    const newUser = await UserService.signUp({ email: 'testing2@email.com', password: '123456' });
+    const res = await agent.put(`/api/v1/users/${newUser.id}`).send({
+      appGoal: 2
+    });
+    expect(res.status).toEqual(403);
   });
   afterAll(() => {
     pool.end();
