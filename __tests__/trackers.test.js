115bfbf834a871ef25657c96473797a59bf3270f
aidanliddiard <aidanliddiard@gmail.com>
2022-07-14 12:34:22 -0700

    setup update route and test is passing

--- __tests__/trackers.test.js
+++ __tests__/trackers.test.js
@@ -92,6 +92,38 @@ describe('tracker routes', () => {
       interviewedAt: null,
     });
   });
+  test('UPDATE to /api/v1/trackers/:id user should update details about job', async () => {
+    const [agent, user] = await signInAndUp();
+    const post = await agent.post('/api/v1/trackers').send({
+      position: 'Software Developer 1',
+      company: 'Amazon',
+      status: 'Applied',
+      notes:
+        'Really like the flexibility of the job and the contact was super helpful.',
+    });
+
+    const resp = await agent.put(`/api/v1/trackers/${post.body.id}`).send({
+      status: 'Interviewing',
+      notes:
+        'Really like the flexibility of the job and the contact was super helpful. Need to practice binary tree code challenges for upcoming interview',
+    });
+
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      userId: user.id,
+      position: 'Software Developer 1',
+      company: 'Amazon',
+      status: 'Interviewing',
+      createdAt: expect.any(String),
+      appliedAt: null,
+      closedAt: null,
+      description: null,
+      notes:
+        'Really like the flexibility of the job and the contact was super helpful. Need to practice binary tree code challenges for upcoming interview',
+      interviewedAt: null,
+    });
+  });
   afterAll(() => {
     pool.end();
   });
